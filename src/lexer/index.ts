410fafeab7d7d10b30de802e6224b29e099b6ad3
0xLogN <nutdriver716@gmail.com>
2023-02-05 08:56:15 -0800

    refactor(lexer): use an object for some functions

--- src/lexer/index.ts
+++ src/lexer/index.ts
@@ -62,7 +62,7 @@ const multiCharOperators = ["==", "!=", ">=", "<=", "+=", "-=", "*=", "/=", "++"
 const panicTokenBoundaries = ['"', ...singleCharOperators];
 
 export default function lex(input: string): LexerOutput {
-    const tokens: Token[] = [];
+    const output: Token[] = [];
     const issues: ZircoIssue<ZircoIssueTypes>[] = [];
     let i = 0;
     // Used to detect infinite loops.
@@ -82,12 +82,16 @@ export default function lex(input: string): LexerOutput {
         issues.push(issue);
     }
 
-    // this explains why we have a separate function for this:
+    // After multiple debates around whether to use getters or a function for current(), the only method
+    // of preventing type narrowing in if statements appears to be a function.
+    // Please read the TypeScript parser itself for more information:
     // https://github.com/microsoft/TypeScript/blob/1ed0a5ac4b49971e4698d5388f7760cbaa0615ac/src/compiler/parser.ts#L2102-L2107
-    const current = () => input[i];
-    const consume = () => input[i++];
-    const peek = () => input[i + 1];
-    const atEOF = () => i >= length;
+    const tokens = {
+        current: () => input[i],
+        consume: () => input[i++],
+        next: () => input[i + 1],
+        atEOF: () => i >= length
+    };
 
     lexerLoop: for (; i < length; ) {
         /* istanbul ignore next */
@@ -98,8 +102,8 @@ export default function lex(input: string): LexerOutput {
         previousI = i;
 
         // Skip whitespace
-        if (/\s/.test(current())) {
-            consume();
+        if (/\s/.test(tokens.current())) {
+            tokens.consume();
             continue;
         }
 
@@ -108,11 +112,11 @@ export default function lex(input: string): LexerOutput {
         // a double quote (unless escaped with a backslash)
 
         // If the first character is a ", then we know it's a string.
-        if (current() === '"') {
+        if (tokens.current() === '"') {
             const start = i;
-            let str = consume();
+            let str = tokens.consume();
 
-            if (atEOF()) {
+            if (tokens.atEOF()) {
                 addIssue(
                     new ZircoSyntaxError(
                         ZircoSyntaxErrorTypes.UnclosedString,
@@ -127,29 +131,29 @@ export default function lex(input: string): LexerOutput {
             }
 
             // We need to loop through the string until we find the end.
-            while (current() !== '"') {
-                if (current() === "\\") {
+            while (tokens.current() !== '"') {
+                if (tokens.current() === "\\") {
                     // This will add the next character to the string, regardless of what it is, even if it's a ".
-                    str += consume();
-                    if (atEOF()) {
+                    str += tokens.consume();
+                    if (tokens.atEOF()) {
                         addIssue(new ZircoSyntaxError(ZircoSyntaxErrorTypes.UnclosedString, { start: i, end: i }, {}));
                         break lexerLoop;
                     }
                 }
 
-                str += consume();
+                str += tokens.consume();
 
-                if (atEOF()) {
+                if (tokens.atEOF()) {
                     addIssue(new ZircoSyntaxError(ZircoSyntaxErrorTypes.UnclosedString, { start, end: i }, {}));
                     break lexerLoop;
                 }
             }
 
             // Consume the final closing quote
-            str += consume();
+            str += tokens.consume();
 
             // We found the end of the string. Let's add it to the output.
-            tokens.push([`${str}`, { type: TokenTypes.String, position: { start, end: i - 1 } }]);
+            output.push([`${str}`, { type: TokenTypes.String, position: { start, end: i - 1 } }]);
             continue;
         }
 
@@ -161,26 +165,26 @@ export default function lex(input: string): LexerOutput {
         // "Prefixed" constants like 0xFF.
         // More specific comes first, so let's start with prefixed.
         // We only support 0b and 0x prefixes, so we can first check for a 0.
-        if (current() === "0" && ["x", "b"].includes(peek())) {
+        if (tokens.current() === "0" && ["x", "b"].includes(tokens.next())) {
             const start = i;
-            let str = consume();
-            const typeOfLiteral = current() === "x" ? "hexadecimal" : "binary";
-            str += consume();
+            let str = tokens.consume();
+            const typeOfLiteral = tokens.current() === "x" ? "hexadecimal" : "binary";
+            str += tokens.consume();
 
-            if (atEOF()) {
+            if (tokens.atEOF()) {
                 addIssue(new ZircoSyntaxError(ZircoSyntaxErrorTypes.NumberPrefixWithNoValue, { start: i, end: i }, { typeOfLiteral }));
                 break lexerLoop;
             }
 
             const matchReg = typeOfLiteral === "binary" ? /[01]/ : /[0-9a-fA-F]/;
 
-            while (/[a-zA-Z0-9]/.test(current())) {
-                if (!matchReg.test(current())) {
+            while (/[a-zA-Z0-9]/.test(tokens.current())) {
+                if (!matchReg.test(tokens.current())) {
                     addIssue(
                         new ZircoSyntaxError(
                             ZircoSyntaxErrorTypes.NumberInvalidCharacter,
                             { start: i, end: i },
-                            { typeOfLiteral, invalidCharacter: current() }
+                            { typeOfLiteral, invalidCharacter: tokens.current() }
                         )
                     );
                     i++;
@@ -188,27 +192,27 @@ export default function lex(input: string): LexerOutput {
                     continue lexerLoop;
                 }
 
-                str += consume();
-                if (atEOF()) break;
+                str += tokens.consume();
+                if (tokens.atEOF()) break;
             }
 
-            tokens.push([str, { type: TokenTypes.Number, position: { start, end: i - 1 } }]);
+            output.push([str, { type: TokenTypes.Number, position: { start, end: i - 1 } }]);
             continue;
         }
 
         // Non-prefixed numbers
-        if (/\d/.test(current())) {
+        if (/\d/.test(tokens.current())) {
             let str = "";
             const start = i;
             let numberOfDecimalPointsEncountered = 0;
 
-            while (/[0-9A-Za-z._]/.test(current())) {
-                if (/[^0-9._]/.test(current())) {
+            while (/[0-9A-Za-z._]/.test(tokens.current())) {
+                if (/[^0-9._]/.test(tokens.current())) {
                     addIssue(
                         new ZircoSyntaxError(
                             ZircoSyntaxErrorTypes.NumberInvalidCharacter,
                             { start: i, end: i },
-                            { typeOfLiteral: "decimal", invalidCharacter: current() }
+                            { typeOfLiteral: "decimal", invalidCharacter: tokens.current() }
                         )
                     );
                     i++;
@@ -216,10 +220,10 @@ export default function lex(input: string): LexerOutput {
                     continue lexerLoop;
                 }
 
-                if (current() === ".") numberOfDecimalPointsEncountered++;
+                if (tokens.current() === ".") numberOfDecimalPointsEncountered++;
 
-                str += consume();
-                if (atEOF()) break;
+                str += tokens.consume();
+                if (tokens.atEOF()) break;
             }
 
             if (numberOfDecimalPointsEncountered > 1) {
@@ -238,32 +242,32 @@ export default function lex(input: string): LexerOutput {
                 continue;
             }
 
-            tokens.push([str, { type: TokenTypes.Number, position: { start, end: i - 1 } }]);
+            output.push([str, { type: TokenTypes.Number, position: { start, end: i - 1 } }]);
             continue;
         }
 
         // Test single-line comments first
-        if (current() + peek() === "//") {
+        if (tokens.current() + tokens.next() === "//") {
             do
-                consume(); // Keep running forwards
-            while (!atEOF() && current() !== "\n");
+                tokens.consume(); // Keep running forwards
+            while (!tokens.atEOF() && tokens.current() !== "\n");
 
             continue;
         }
 
         {
             // Then block comments
-            const isBlockCommentOpening = () => current() + peek() === "/*",
-                isBlockCommentClosing = () => current() + peek() === "*/";
+            const isBlockCommentOpening = () => tokens.current() + tokens.next() === "/*",
+                isBlockCommentClosing = () => tokens.current() + tokens.next() === "*/";
 
             if (isBlockCommentOpening()) {
                 const start = i;
-                consume(); // Increment once to prevent something like /*/ from counting (prevent the asterisk from being counted twice)
+                tokens.consume(); // Increment once to prevent something like /*/ from counting (prevent the asterisk from being counted twice)
 
                 let nest = 1;
 
                 do {
-                    consume();
+                    tokens.consume();
 
                     // if we hit another block comment, increment the nest level and skip forwards
                     if (isBlockCommentOpening()) nest++;
@@ -274,9 +278,9 @@ export default function lex(input: string): LexerOutput {
                         // If we hit a */ group that closes the outermost block, exit
                         if (nest === 0) break;
                     }
-                } while (!atEOF());
+                } while (!tokens.atEOF());
 
-                if (atEOF()) {
+                if (tokens.atEOF()) {
                     addIssue(
                         new ZircoSyntaxError(
                             ZircoSyntaxErrorTypes.UnclosedBlockComment,
@@ -289,8 +293,8 @@ export default function lex(input: string): LexerOutput {
                     );
                     break;
                 } else {
-                    consume(); // skip the *
-                    consume(); // then skip the /
+                    tokens.consume(); // skip the *
+                    tokens.consume(); // then skip the /
                     continue;
                 }
             }
@@ -299,16 +303,16 @@ export default function lex(input: string): LexerOutput {
         // At this point, names shouldn't be too difficult.
         // Names are just a sequence of letters, numbers, and underscores.
         // They can't start with a number, though.
-        if (/[a-zA-Z_]/.test(current())) {
+        if (/[a-zA-Z_]/.test(tokens.current())) {
             let str = "";
             const start = i;
 
-            while (/[a-zA-Z0-9_]/.test(current())) {
-                str += consume();
-                if (atEOF()) break;
+            while (/[a-zA-Z0-9_]/.test(tokens.current())) {
+                str += tokens.consume();
+                if (tokens.atEOF()) break;
             }
 
-            tokens.push([str, { type: TokenTypes.Name, position: { start, end: i - 1 } }]);
+            output.push([str, { type: TokenTypes.Name, position: { start, end: i - 1 } }]);
             continue;
         }
 
@@ -331,7 +335,7 @@ export default function lex(input: string): LexerOutput {
 
             if (didMatchCurrent) {
                 // We found a match! Let's add it to the output.
-                tokens.push([op, { type: TokenTypes.Operator, position: { start: i, end: i + op.length - 1 } }]);
+                output.push([op, { type: TokenTypes.Operator, position: { start: i, end: i + op.length - 1 } }]);
                 i += op.length;
                 continue lexerLoop;
             }
@@ -340,14 +344,14 @@ export default function lex(input: string): LexerOutput {
         // Single-character operators
         // These are operators that are only one character long.
         // Examples of these are +, -, *, /, etc.
-        if (singleCharOperators.includes(current())) {
-            tokens.push([consume(), { type: TokenTypes.Operator, position: { start: i - 1, end: i - 1 } }]);
+        if (singleCharOperators.includes(tokens.current())) {
+            output.push([tokens.consume(), { type: TokenTypes.Operator, position: { start: i - 1, end: i - 1 } }]);
             continue;
         }
 
         // Anything else is an OTHER
-        tokens.push([consume(), { type: TokenTypes.Other, position: { start: i - 1, end: i - 1 } }]);
+        output.push([tokens.consume(), { type: TokenTypes.Other, position: { start: i - 1, end: i - 1 } }]);
     }
     if (issues.length) return { ok: false, issues, tokens: null };
-    else return { ok: true, issues: [], tokens };
+    else return { ok: true, issues: [], tokens: output };
 }
